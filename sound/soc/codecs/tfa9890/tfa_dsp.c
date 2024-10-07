df7d8f36daf86151a1425ed6b36768747a3cdc2e
Demon000 <demonsingur@gmail.com>
2024-10-07 21:32:20 +0700

    sound: codecs: tfa9890: add error translation routine
    Clang warns:
    sound/soc/codecs/tfa9890/tfa_dsp.c:3093:9: warning: implicit conversion from enumeration type 'enum Tfa98xx_Error' to different enumeration type 'enum tfa_error' [-Wenum-conversion]
            return err;
            ~~~~~~ ^~~
    sound/soc/codecs/tfa9890/tfa_dsp.c:3125:9: warning: implicit conversion from enumeration type 'enum Tfa98xx_Error' to different enumeration type 'enum tfa_error' [-Wenum-conversion]
            return err;
            ~~~~~~ ^~~
    sound/soc/codecs/tfa9890/tfa_dsp.c:3177:9: warning: implicit conversion from enumeration type 'enum Tfa98xx_Error' to different enumeration type 'enum tfa_error' [-Wenum-conversion]
            return err;
            ~~~~~~ ^~~
    
    Change-Id: I4d1d428f0306bbe6bb25d8beab8a74307aaeadf7

--- sound/soc/codecs/tfa9890/tfa_dsp.c
+++ sound/soc/codecs/tfa9890/tfa_dsp.c
@@ -2943,6 +2943,24 @@ enum Tfa98xx_Error tfaRunWaitCalibration(Tfa98xx_handle_t handle, int *calibrate
 	return err;
 }
 
+enum tfa_error tfa98xxTotfa(enum Tfa98xx_Error err)
+{
+	switch(err) {
+	case Tfa98xx_Error_Ok:
+		return tfa_error_ok;
+	case Tfa98xx_Error_Device:
+		return tfa_error_device;
+	case Tfa98xx_Error_Bad_Parameter:
+		return tfa_error_bad_param;
+	case Tfa98xx_Error_NoClock:
+		return tfa_error_noclock;
+	case Tfa98xx_Error_StateTimedOut:
+		return tfa_error_timeout;
+	default:
+		return tfa_error_bad_param;
+	}
+}
+
 enum tfa_error tfa_start(int next_profile, int *vstep, int dev)
 {
 	enum Tfa98xx_Error err = Tfa98xx_Error_Ok;
@@ -3090,7 +3108,7 @@ error_exit:
 		tfaRunUnmute(dev);	/* unmute */
 		tfaContClose(dev); /* close all of them */
 
-	return err;
+	return tfa98xxTotfa(err);
 }
 
 enum tfa_error tfa_stop(int dev)
@@ -3122,7 +3140,7 @@ enum tfa_error tfa_stop(int dev)
 
 error_exit:
 		tfaContClose(dev); /* close all of them */
-	return err;
+	return tfa98xxTotfa(err);
 }
 
 /*
@@ -3174,7 +3192,7 @@ enum tfa_error tfa_reset(void)
 		tfaContClose(dev);
 	}
 
-	return err;
+	return tfa98xxTotfa(err);
 }
 
 /*
